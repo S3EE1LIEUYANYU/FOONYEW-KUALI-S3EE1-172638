fb7f00a730d0c4b06e2d39f460a0c72a01963500
S3EE1LIEUYANYU <104434109+S3EE1LIEUYANYU@users.noreply.github.com>
2022-04-26 21:33:19 +0800

    Update ex1-1.cpp

--- ex1-1.cpp
+++ ex1-1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
+    cout<<"Hello Albee\n";//她是一个学霸（学习很厉害的意思）
+    printf("My height is %d m %d cm \n",1,80);//我的身高是1米8
 
     return 0;
 }
