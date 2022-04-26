ee3a441f2a68f4adb0e6c0859b1c1d8add5ad0e8
S3EE1LIEUYANYU <104434109+S3EE1LIEUYANYU@users.noreply.github.com>
2022-04-26 21:42:06 +0800

    Create ex2-1.cpp

---
+++ ex2-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello Albee\n";//她是一个学霸（学习很厉害的意思）
+    cout<<"My height is " << 1 << " m " << 80 << " cm ";//我的身高是1米8
+
+    return 0;
+}
