42d6b4284510ea2d31d321515a13642cf97cbd63
S3EE1LIEUYANYU <104434109+S3EE1LIEUYANYU@users.noreply.github.com>
2022-05-03 21:54:40 +0800

    Update ex2-2.cpp

--- ex2-2.cpp
+++ ex2-2.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+
+    float   i;
+    float   pi=3.1415926535897932384626433832795;
+    
+    printf(" please input the radius(cm)\n");
+    scanf("%f",&i);
+    
+    printf(" the circle area is %f cm^2 \n",pi*i*i);
+    printf(" the circumference is %f cm \n",2*pi*i);
+    
+    return 0;
+}
