8d32d4840be9ab9b2fbcf27cad79aa41a8a19b6c
S3EE1LIEUYANYU <104434109+S3EE1LIEUYANYU@users.noreply.github.com>
2022-05-03 21:20:41 +0800

    Create exe2.-1.cpp

---
+++ exe2.-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int     i,j;
+    
+    printf("请输入第一个数字\n");
+    scanf("%d",&i);
+    printf("请输入第二个数字\n");
+    scanf("%d",&j);
+    
+    printf("两数之平方和=%d\n",(i*i)+(j*j));
+    
+    return 0;
+}
