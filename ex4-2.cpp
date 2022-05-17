8d8fed6560d36079a241940d55e56b6873bf3c08
S3EE1LIEUYANYU <104434109+S3EE1LIEUYANYU@users.noreply.github.com>
2022-05-17 20:54:28 +0800

    Update ex4-2.cpp

--- ex4-2.cpp
+++ ex4-2.cpp
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+    float r;
+    float pi=3.1415926535897932384626433832795;
+    int y;
+    
+    printf("请输入半径=\n");
+    scanf("%f",&r);
+    
+    if( r >= 0 ){
+    printf("圆面积 %f \n",r * pi);
+   }
+   else{
+    printf("无效半径 \n");
+   }
+   
+    printf("请输入年份=\n");
+    scanf("%d",&y);
+    
+   if( y % 4 == 0){
+       printf("%d年是闰年 \n",y);
+   }
+   else{
+       printf("%d年不是润年 \n",y);
+   }
+    
+    
+    cout<<"Hello World";
+
+    return 0;
+}
