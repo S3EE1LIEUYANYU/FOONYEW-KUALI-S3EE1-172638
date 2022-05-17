fb9940df0c72eb6efa4bde1880aa49c464b10b4d
S3EE1LIEUYANYU <104434109+S3EE1LIEUYANYU@users.noreply.github.com>
2022-05-17 21:31:14 +0800

    Update ex4-1.cpp

--- ex4-1.cpp
+++ ex4-1.cpp
@@ -5,20 +5,13 @@ using namespace std;
 
 int main()
 {
-    int x,y;
-    // int bool_value;
+    int x,y,z;
     
     printf(" please input x=\n");
     scanf("%d",&x);
     printf(" please input y=\n");
     scanf("%d",&y);
-    
-    bool_value = ( x > y );
-    printf(" x > y ==> %d \n",bool_value);
-    bool_value = ( x < y );
-    printf(" x < y ==> %d \n",bool_value);
-    bool_value = ( x < 5 or x > 10);
-    printf(" x < 5 or x > 10 ==> %d \n",bool_value);
+
     
    if( x > y ){
     printf(" x > y ==> %d \n",x > y);
@@ -29,6 +22,9 @@ int main()
    else{
     printf(" x = y \n");
    }
+   
+   z = ( x > y ) ? x : y ;
+   printf("z = %d \n",z);
     
     
     
