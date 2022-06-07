d141fcf55a22d80d696b58cc6437a2f75f99663d
S3EE1LIEUYANYU <172638lie@fyk.edu.my>
2022-06-07 13:15:50 +0000

    update

--- ex7-4.cpp
+++ ex7-4.cpp
@@ -1,15 +1,25 @@
 #include <stdio.h>
+int minutesCount(float t1, int t2);
 
 int main()
 {
     float t1;
     int t2;
-    printf("开始\n");
+    float minutes;
+    printf("请输入 XX 小时 与 XX 分钟\n");
     scanf("%f",&t1);
     printf("小时\n");
     scanf("%d",&t2);
     printf("分钟\n");
     printf("约等于\n");
-    printf("%f 分钟",t2+(t1*60));
+    minutes = minutesCount(float t1, int t2);
+    printf("%f 分钟",minutes);
     return 0;
+}
+
+int minutesCount(float t1, int t2)
+{
+    float result;
+    result = t2+(t1*60);
+    return result; 
 }
