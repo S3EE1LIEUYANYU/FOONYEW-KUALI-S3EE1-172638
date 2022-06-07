1d7273ee7b6a8fbacbae815dd7cf734d05b47028
S3EE1LIEUYANYU <172638lie@fyk.edu.my>
2022-06-07 13:47:35 +0000

    update

--- ex7-5.cpp
+++ ex7-5.cpp
@@ -1,14 +1,13 @@
 #include <stdio.h>
-int hoursCount(float t1, int t2);
+int hoursCount(float t1, float t2);
 
 int main()
 {
-    float t1,hours;
-    int t2;
+    float t1,t2,hours;
     printf("请输入 XX 小时 与 XX 分钟\n");
     scanf("%f",&t1);
     printf("小时\n");
-    scanf("%d",&t2);
+    scanf("%f",&t2);
     printf("分钟\n");
     printf("约等于\n");
     hours = hoursCount(t1,t2);
@@ -16,7 +15,7 @@ int main()
     return 0;
 }
 
-int hoursCount(float t1, int t2)
+int hoursCount(float t1, float t2)
 {
     float result;
     result = (t1*60+t2)/60;
