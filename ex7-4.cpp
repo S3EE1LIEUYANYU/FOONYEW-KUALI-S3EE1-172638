0849a2f5bf07635324f616d7a4b08013e1870132
S3EE1LIEUYANYU <172638lie@fyk.edu.my>
2022-06-07 13:08:25 +0000

    update

--- ex7-4.cpp
+++ ex7-4.cpp
@@ -6,9 +6,10 @@ int main()
     int t2;
     printf("开始\n");
     scanf("%f",&t1);
-    printf(" 小时");
+    printf("小时");
     scanf("%d",&t2);
-    printf(" 分钟 约等于 ");
+    printf("分钟\n");
+    printf("约等于\n")
     printf("%f 分钟",t2+(t1*60));
     return 0;
 }
