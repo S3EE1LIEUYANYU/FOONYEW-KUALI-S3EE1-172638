0bec7c52579d00da594dedcf2cedeef63f6c6f3c
S3EE1LIEUYANYU <172638lie@fyk.edu.my>
2022-06-07 13:02:27 +0000

    update

---
+++ ex7-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2,mianji;
+    printf("Enters two numbers: ");
+    scanf("%d %d",&n1,&n2);
+    mianji = n1*n2;  // function call
+    printf("mianji = %d \n",mianji);
+    return 0;
+}
---
+++ ex7-4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int t1,t2;
+    printf("开始\n");
+    scanf(%d,&t1);
+    printf(" 小时");
+    scanf(%d,&t2);
+    printf(" 分钟 约等于 ");
+    printf("%d 分钟",t2+(t1*60));
+    return 0;
+}
