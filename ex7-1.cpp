d8e4bd738a9ee84ac1bfb232149e81b84bc276a8
S3EE1LIEUYANYU <172638lie@fyk.edu.my>
2022-06-07 12:09:38 +0000

    update

--- ex7-1.cpp
+++ ex7-1.cpp
@@ -10,7 +10,7 @@ int main ()
     f_num = pow(8.0, i_num);
     printf("  %f \n",f_num);
     printf("Square root of %lf is %lf \n",4.0,sqrt(4.0));
-    printf("Square root of %lf is %lf \n",5.0,sqrt(5.0));
+    printf("Square root of %lf is %lf \n",9.0,sqrt(9.0));
 
     printf("Square root of %lf is %lf \n", 4.0, f_num);
     return(0);
