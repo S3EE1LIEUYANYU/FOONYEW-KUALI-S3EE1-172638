382a7324afea3935c64530919caed1dc3b11eb18
S3EE1LIEUYANYU <172638lie@fyk.edu.my>
2022-06-21 13:11:16 +0000

    uodate

--- ex8-1.cpp
+++ ex8-1.cpp
@@ -3,34 +3,34 @@
 // using namespace std;
 
 // int main(){
-    // int b = 10, c = 1;
-    // int a[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,100};
+//     int b = 10, c = 1;
+//     int a[]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,100};
 
-    // int *p;
+//     int *p;
     
-    // for (int i=0;i<sizeof(a)/sizeof(int);i++){
-    //     p=a;
-    //     printf("a[%d] = %x \n",i,*(p+i));
-    //     printf("a[%d] = %x \n",i,p[i]);
-    // }
+//     for (int i=0;i<sizeof(a)/sizeof(int);i++){
+//         p=a;
+//         printf("a[%d] = %x \n",i,*(p+i));
+//         printf("a[%d] = %x \n",i,p[i]);
+//     }
 
-    // p=a;
-    // printf("a[0] = %x \n",p[0]);
+//     p=a;
+//     printf("a[0] = %x \n",p[0]);
 
-    // printf("b = \n");
-    // scanf("%d",&b);
-    // printf("b = %d \n",b);
-    // printf("&b = %x \n",&b);
-
-    // p = &b;
-    // printf("b = %d \n",*p);
-    // printf("&b = %x \n",p);
+//     printf("b = \n");
+//     scanf("%d",&b);
+//     printf("b = %d \n",b);
+//     printf("&b = %x \n",&b);
+// // 
+//     p = &b;
+//     printf("b = %d \n",*p);
+//     printf("&b = %x \n",p);
     
-    // p = &c;
-    // printf("c = %d \n",*p);
-    // printf("&c = %x \n",p);
+//     p = &c;
+//     printf("c = %d \n",*p);
+//     printf("&c = %x \n",p);
 
-    cout << endl << "Hello World";
+//     cout << endl << "Hello World";
 
-    return 0;
-}
+//     return 0;
+// }
